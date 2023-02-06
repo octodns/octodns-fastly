a59ee96baa455da35b84d3a94325862a84bc22c9
Samuel Parkinson <samuel.parkinson@ft.com>
2023-02-06 17:16:35 +0000

    Add initial implmentation

--- octodns_fastly/__init__.py
+++ octodns_fastly/__init__.py
@@ -1,12 +1,15 @@
-#
-#
-#
+"""
+A Fastly source for OctoDNS.
+
+Automatically create ACME CNAME records for verifying domains in Fastly TLS subscriptions.
+"""
 
 import logging
 
 import fastly
 from octodns.record import Record
 from octodns.source.base import BaseSource
+from octodns.zone import Zone
 
 
 class FastlyAcmeSource(BaseSource):
@@ -32,16 +35,56 @@ class FastlyAcmeSource(BaseSource):
     SUPPORTS_DYNAMIC = False
     SUPPORTS = set(("CNAME"))
 
-    def __init__(self, id, token):
+    DEFAULT_TTL = 3600
+
+    def __init__(self, id: str, token: str, ttl: int = DEFAULT_TTL):
         klass = self.__class__.__name__
         self.log = logging.getLogger(f"{klass}[{id}]")
-        self.log.debug("__init__: id=%s", id)
+        self.log.debug("__init__: id=%s, ttl=%d", id, ttl)
+
         super().__init__(id)
 
+        self.ttl = ttl
         self._config = fastly.Configuration()
         self._config.api_token = token
 
-    def populate(self, zone, target=False, lenient=False):
+        self._challenges = None
+
+    def challenges(self, zone: Zone):
+        if self._challenges is None:
+            with fastly.ApiClient(self._config) as client:
+                instance = fastly.api.tls_subscriptions_api.TlsSubscriptionsApi(client)
+                subscriptions = instance.list_tls_subs(
+                    include="tls_authorizations", filter_tls_domains_id=zone.name.removesuffix(".")
+                )
+
+                if subscriptions["meta"]["total_pages"] > 1:
+                    raise NotImplementedError("More than one page of TLS subscriptions is not supported")
+
+                # Ensure we only have a list of authorizations
+                authorizations = [
+                    authorization
+                    for authorization in subscriptions["included"]
+                    if authorization["type"] == "tls_authorization"
+                ]
+
+                challenges = []
+
+                for authorization in authorizations:
+                    for challenge in authorization["attributes"]["challenges"]:
+                        if challenge["type"] == "managed-dns":
+                            challenges.append(
+                                {
+                                    "name": challenge["record_name"].removesuffix(".example.com"),
+                                    "value": "%s." % challenge["values"][0],
+                                }
+                            )
+
+                self._challenges = challenges
+
+        return self._challenges
+
+    def populate(self, zone: Zone, target=False, lenient=False):
         self.log.debug(
             "populate: name=%s, target=%s, lenient=%s",
             zone.name,
@@ -51,19 +94,16 @@ class FastlyAcmeSource(BaseSource):
 
         before = len(zone.records)
 
-        # with fastly.ApiClient(self._config) as client:
-        #     instance = fastly.api.TlsSubscriptionsApi(client)
-        #     try:
-        #         subscriptions = instance.list_tls_subs()
-        #     except fastly.ApiException as e:
-        #         self.log.error("populate:   error=%s", e)
-
-        record = Record.new(
-            zone,
-            "_acme-challenge",
-            {"ttl": 60, "type": "CNAME", "value": "example.com"},
-            source=self,
-        )
-        zone.add_record(record)
+        for challange in self.challenges(zone):
+            record = Record.new(
+                zone,
+                challange["name"],
+                {
+                    "type": "CNAME",
+                    "ttl": self.ttl,
+                    "value": challange["value"],
+                },
+            )
+            zone.add_record(record)
 
         self.log.info("populate:   found %s records", len(zone.records) - before)
--- tests/test_octodns_source_fastly.py
+++ tests/test_octodns_source_fastly.py
@@ -1,9 +1,212 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+from fastly.api.tls_subscriptions_api import TlsSubscriptionsApi
+from octodns.zone import Zone
 
 from octodns_fastly import FastlyAcmeSource
 
 
-class TestFastlyAcmeSource(TestCase):
+class FastlyAcmeSourceTestCase(TestCase):
     def test_init(self):
-        source = FastlyAcmeSource("test")
-        self.assertEqual(source.id, "test")
+        source = FastlyAcmeSource("test_id", "test_token")
+        assert source.id == "test_id"
+        assert source.ttl == 3600
+        assert source._config.api_token == "test_token"
+
+    def test_custom_default_ttl(self):
+        zone = Zone("example.com.", [])
+        source = FastlyAcmeSource("test_id", "test_token", ttl=60)
+
+        with patch.object(TlsSubscriptionsApi, "list_tls_subs") as mock:
+            mock.return_value = {
+                "data": [],
+                "included": [
+                    {
+                        "type": "tls_authorization",
+                        "attributes": {
+                            "challenges": [
+                                {
+                                    "type": "managed-dns",
+                                    "record_type": "CNAME",
+                                    "record_name": "_acme-challenge.example.com",
+                                    "values": ["1234567890abcdef.fastly-validations.com"],
+                                }
+                            ]
+                        },
+                    }
+                ],
+                "meta": {"total_pages": 1},
+            }
+
+            source.populate(zone)
+
+            assert len(zone.records) == 1
+
+            records = {(r.name, r._type): r for r in zone.records}
+
+            assert 60 == records[("_acme-challenge", "CNAME")].ttl
+
+    def test_challanges_filters_by_zone(self):
+        zone = Zone("example.net.", [])
+        source = FastlyAcmeSource("test_id", "test_token", ttl=60)
+
+        with patch.object(TlsSubscriptionsApi, "list_tls_subs") as mock:
+            mock.return_value = {
+                "data": [],
+                "included": [],
+                "meta": {"total_pages": 1},
+            }
+
+            source.populate(zone)
+
+            assert len(zone.records) == 0
+            mock.assert_called_with(include="tls_authorizations", filter_tls_domains_id="example.net")
+
+    def test_populate_with_no_tls_subscriptions(self):
+        zone = Zone("example.com.", [])
+        source = FastlyAcmeSource("test_id", "test_token", ttl=60)
+
+        with patch.object(TlsSubscriptionsApi, "list_tls_subs") as mock:
+            mock.return_value = {
+                "data": [],
+                "included": [],
+                "meta": {"total_pages": 1},
+            }
+
+            source.populate(zone)
+
+            assert len(zone.records) == 0
+
+    def test_populate_with_single_tls_challenge(self):
+        zone = Zone("example.com.", [])
+        source = FastlyAcmeSource("test_id", "test_token")
+
+        with patch.object(TlsSubscriptionsApi, "list_tls_subs") as mock:
+            mock.return_value = {
+                "data": [],
+                "included": [
+                    {
+                        "type": "tls_authorization",
+                        "attributes": {
+                            "challenges": [
+                                {
+                                    "type": "managed-dns",
+                                    "record_type": "CNAME",
+                                    "record_name": "_acme-challenge.www.example.com",
+                                    "values": ["1234567890abcdef.fastly-validations.com"],
+                                }
+                            ]
+                        },
+                    }
+                ],
+                "meta": {"total_pages": 1},
+            }
+
+            source.populate(zone)
+
+            records = {(r.name, r._type): r for r in zone.records}
+            record = records[("_acme-challenge.www", "CNAME")]
+
+            assert len(zone.records) == 1
+            assert "_acme-challenge.www" == record.name
+            assert "CNAME" == record._type
+            assert "1234567890abcdef.fastly-validations.com." == record.value
+            assert 3600 == record.ttl
+
+    def test_populate_with_multiple_tls_challenges(self):
+        zone = Zone("example.com.", [])
+        source = FastlyAcmeSource("test_id", "test_token")
+
+        with patch.object(TlsSubscriptionsApi, "list_tls_subs") as mock:
+            mock.return_value = {
+                "data": [],
+                "included": [
+                    {
+                        "type": "tls_authorization",
+                        "attributes": {
+                            "challenges": [
+                                {
+                                    "type": "managed-dns",
+                                    "record_type": "CNAME",
+                                    "record_name": "_acme-challenge.example.com",
+                                    "values": ["1234567890abcdef.fastly-validations.com"],
+                                },
+                                {
+                                    "type": "managed-dns",
+                                    "record_type": "CNAME",
+                                    "record_name": "_acme-challenge.www.example.com",
+                                    "values": ["fedcba0987654321.fastly-validations.com"],
+                                },
+                            ]
+                        },
+                    }
+                ],
+                "meta": {"total_pages": 1},
+            }
+
+            source.populate(zone)
+
+            records = {(r.name, r._type): r for r in zone.records}
+
+            assert len(zone.records) == 2
+
+            record = records[("_acme-challenge", "CNAME")]
+            assert "_acme-challenge" == record.name
+            assert "CNAME" == record._type
+            assert "1234567890abcdef.fastly-validations.com." == record.value
+            assert 3600 == record.ttl
+
+            record = records[("_acme-challenge.www", "CNAME")]
+            assert "_acme-challenge.www" == record.name
+            assert "CNAME" == record._type
+            assert "fedcba0987654321.fastly-validations.com." == record.value
+            assert 3600 == record.ttl
+
+    def test_populate_with_multiple_tls_subscriptions(self):
+        zone = Zone("example.com.", [])
+        source = FastlyAcmeSource("test_id", "test_token")
+
+        with patch.object(TlsSubscriptionsApi, "list_tls_subs") as mock:
+            mock.return_value = {
+                "data": [],
+                "included": [
+                    {
+                        "type": "tls_authorization",
+                        "attributes": {
+                            "challenges": [
+                                {
+                                    "type": "managed-dns",
+                                    "record_type": "CNAME",
+                                    "record_name": "_acme-challenge.www.example.com",
+                                    "values": ["1234567890abcdef.fastly-validations.com"],
+                                }
+                            ]
+                        },
+                    }
+                ],
+                "meta": {"total_pages": 1},
+            }
+
+            source.populate(zone)
+
+            records = {(r.name, r._type): r for r in zone.records}
+            record = records[("_acme-challenge.www", "CNAME")]
+
+            assert len(zone.records) == 1
+            assert "_acme-challenge.www" == record.name
+            assert "CNAME" == record._type
+            assert "1234567890abcdef.fastly-validations.com." == record.value
+            assert 3600 == record.ttl
+
+    def test_populate_errors_on_too_many_subscriptions(self):
+        zone = Zone("example.com.", [])
+        source = FastlyAcmeSource("test_id", "test_token")
+
+        with patch.object(TlsSubscriptionsApi, "list_tls_subs") as mock:
+            mock.return_value = {"data": [], "included": [], "meta": {"total_pages": 2}}
+
+            with self.assertRaises(NotImplementedError) as context:
+                source.populate(zone)
+
+            assert "More than one page of TLS subscriptions is not supported" in str(context.exception)
